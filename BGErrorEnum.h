5e6e79b76297148117471d053bf7aab809f9a0a1
Jeon <sodtms@naver.com>
2018-08-01 10:51:50 +0900

    [1.04] BGProtoType 적용 : 에러타입 처리 관련 개선사항

--- BGErrorEnum.h
+++ BGErrorEnum.h
@@ -1,13 +1,19 @@
 #pragma once
 
-enum class CommonError {
-	BG_NONE,	// 정상
-	BG_ERROR,	// 에러	
+class BG_ERROR {
+public:
+	enum class Common {
+		BG_NONE,	// 정상
+		BG_ERROR,	// 에러	
+	};
+
+	enum class Http {
+		BG_NONE,	// 정상
+		BG_ERROR,	// 에러
+	};
+
+	static int ErrorCode(Common error) { return static_cast<int>(error); }
+	static int ErrorCode(Http error) { return static_cast<int>(error); }
 };
 
-enum class HttpError {
-	BG_NONE,	// 정상
-	BG_ERROR,	// 에러
 
-	
-};
